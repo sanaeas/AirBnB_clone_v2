ea1d6789619e6aa98b6d38af3a23c24c075eaf1d
sanaeas <sanaassim1@gmail.com>
2023-10-20 10:47:46 +0100

    display 'C ' followed by the value of the text variable

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Start a Flask web application
+"""
+
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Display a hello message"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Display HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is(text):
+    """display 'C ' followed by the value of the text variable"""
+    return f'C {escape(text.replace("_", " "))}'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
