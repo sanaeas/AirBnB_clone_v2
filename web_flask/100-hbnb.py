a05c9f3f6e9cd041822f9ead40e478d9155364a2
sanaeas <sanaassim1@gmail.com>
2023-10-21 12:25:42 +0100

    Fix: use the right template

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -18,7 +18,7 @@ def hbnb():
     states = storage.all(State)
     amenities = storage.all(Amenity)
     places = storage.all(Place)
-    return render_template('10-hbnb_filters.html', states=states,
+    return render_template('100-hbnb.html', states=states,
                            amenities=amenities, places=places)
 
 
