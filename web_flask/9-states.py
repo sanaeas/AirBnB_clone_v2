e3a0419b239501c53f35685a7053613f6cf935a8
Sana Assim <sanaassim1@gmail.com>
2023-10-21 04:45:20 -0700

    Fix: Add a blank line

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -9,6 +9,7 @@ from models.state import State
 
 app = Flask(__name__)
 
+
 @app.route('/states', strict_slashes=False)
 @app.route('/states/<id>', strict_slashes=False)
 def list_state_cities(id=None):
